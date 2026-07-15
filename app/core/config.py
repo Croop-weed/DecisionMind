c11e3be2efb859faaa0605456ee7a34426a5b9a1
Harshit Dhaundiyal <harsh.arror@gmail.com>
2026-07-15 12:53:44 +0530

    auth

--- app/core/config.py
+++ app/core/config.py
@@ -8,6 +8,9 @@ class Setting(BaseSettings):
     ALEMBIC_DATABASE_URL: str
 
     SECRET_KEY : str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
     DEBUG: bool = False
     APP_NAME: str = "DecisionAI"
 
